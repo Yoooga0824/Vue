a3f6c12796ebcef01b91ea882249c8b8eb8fcfde
Yoooga0824 <17582495726@163.com>
2025-08-06 20:53:47 +0800

    First Save

---
+++ vue_base/src/App.vue
@@ -0,0 +1,19 @@
+<template>  
+  <h3>模板语法</h3>
+  <p>{{ msg }}</p>
+  <p>{{ number + 1 }}</p>
+  <p v-html = "rawHtml"></p>
+</template>
+
+<script>
+export default{
+  data(){
+    return{
+      msg:"Hello Vue3",
+      number:10,
+      rawHtml:"<a href='https://itbaizhan.com'>百战程序员</a>"
+    }
+  }
+}
+</script>
+

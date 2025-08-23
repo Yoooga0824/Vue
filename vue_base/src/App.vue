702bcc56c07a1a94839b1cc537dfe857087ca514
Yoooga0824 <17582495726@163.com>
2025-08-23 10:27:29 +0800

    feat:Some new functions

--- vue_base/src/App.vue
+++ vue_base/src/App.vue
@@ -1,18 +1,28 @@
 <template>
-  <IfDemo />
-  <ListDemo />
-  <div v-bind:class="dynamiccla" v-bind:id="dynamicid">测试</div>
+  <!-- <IfDemo />
+  <ListDemo /> -->
+  <EventDemo1 />
+  <EventDemo2 />
+  <EventDemo3 />
+  <!-- <div v-bind:class="dynamiccla" v-bind:id="dynamicid">测试</div>
   <button :disabled=isornot>Button</button>
-  <div v-bind="objectofattrs"> Test</div>
+  <div v-bind="objectofattrs"> Test</div> -->
 </template>
 
 <script>
 import IfDemo from './components/IfDemo.vue'
 import ListDemo from './components/ListDemo.vue'
+import EventDemo1 from './components/EventDemo1.vue'
+import EventDemo2 from './components/EventDemo2.vue'
+import EventDemo3 from './components/EventDemo3.vue'
+
 export default{
   components:{
     IfDemo,
-    ListDemo
+    ListDemo,
+    EventDemo1,
+    EventDemo2,
+    EventDemo3,
   },
   data(){
     return{
---
+++ vue_base/src/components/EventDemo3.vue
@@ -0,0 +1,24 @@
+<template>
+    <h3>事件传参</h3>
+    <p @click="getName(item,$event)" v-for="(item,index) of names" :key="index">
+        {{ item }}
+    </p>
+</template>
+
+<script>
+
+export default{
+    data(){
+        return{
+            names:["iwen","ime","frank"]
+        }
+    },
+    methods:{
+        getName(name,event){
+            console.log(name);
+            console.log(event);
+        }
+    }
+}
+
+</script>
